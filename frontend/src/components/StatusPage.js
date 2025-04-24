9375553b7f5dcfa67051ae09598ea69d2a129e03
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-24 16:40:27 +0000

    e

---
+++ frontend/src/components/StatusPage.js
@@ -0,0 +1,14 @@
+// frontend/src/components/StatusPage.js
+
+import React from 'react';
+
+const StatusPage = () => {
+  return (
+    <div style={{ padding: '20px', color: 'white' }}>
+      <h2>Status Page</h2>
+      <p>Buyurtmangiz holatini shu yerdan ko‘rishingiz mumkin.</p>
+    </div>
+  );
+};
+
+export default StatusPage;
