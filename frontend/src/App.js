7b2b6b22786e61ec353ec5bd57b329b26c94d957
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-24 22:11:02 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import '../src/styles/App.css';
 import ProductList from './components/ProductList';
 import OrderForm from './components/OrderForm';
 import PaymentPage from './components/PaymentPage';
 import OrderStatus from './components/OrderStatus';
 import StatusPage from './components/StatusPage';
 import AdminPanel from './components/AdminPanel';
-import './styles/App.css';
 
 function App() {
-  const [step, setStep] = useState(1);
-  const [orderItems, setOrderItems] = useState([]);
-  const [orderInfo, setOrderInfo] = useState({});
-  const [orderId, setOrderId] = useState(null);
-
-  const goToNext = (items) => {
-    setOrderItems(items);
-    setStep(2);
-  };
-
-  const handleFormSubmit = (info) => {
-    setOrderInfo(info);
-    setStep(3);
-  };
-
-  const handlePayment = (id) => {
-    setOrderId(id);
-    setStep(4);
-  };
-
-  const goToStatusPage = () => {
-    setStep(5);
-  };
-
   return (
-    <div className="App">
-      {step === 1 && <ProductList onNext={goToNext} />}
-      {step === 2 && <OrderForm items={orderItems} onSubmit={handleFormSubmit} />}
-      {step === 3 && <PaymentPage order={orderInfo} onPay={handlePayment} />}
-      {step === 4 && <OrderStatus orderId={orderId} onViewStatus={goToStatusPage} />}
-      {step === 5 && <StatusPage orderId={orderId} />}
-      {step === 999 && <AdminPanel />}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/order" element={<OrderForm />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/status" element={<OrderStatus />} />
+        <Route path="/order-status/:orderId" element={<StatusPage />} />
+        <Route path="/admin" element={<AdminPanel />} />
+      </Routes>
+    </Router>
   );
 }
 
