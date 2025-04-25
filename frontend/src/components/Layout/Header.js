c66b2133b585f50374705a8eabb764c7df44e30f
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 17:47:08 +0500

    Update Header.js

--- frontend/src/components/Layout/Header.js
+++ frontend/src/components/Layout/Header.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import logo from '../../assets/logo.png';
+// src/components/Layout/Header.js
 
-const Header = () => (
-  <header>
-    <img src={logo} alt="MLBB Diamond Store" />
-    <h1>MLBB Diamond Store</h1>
-  </header>
-);
+import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
+import '../../styles/Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="logo-container">
+        <img src={logo} alt="MLBB Diamond Store" className="logo" />
+      </div>
+      <nav className="nav">
+        <Link to="/">Home</Link>
+        <Link to="/status">Status</Link>
+        <Link to="/admin">Admin</Link>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
