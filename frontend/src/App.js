f4589848dc0c775c9f3a180f23f56c0b937d386a
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:34:22 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import OrderForm from './components/OrderForm';
+import Header from './components/Layout/Header';
+import Footer from './components/Layout/Footer';
+import OrderForm from './pages/OrderForm';
+import OrderSummary from './pages/OrderSummary';
+import PaymentPage from './pages/PaymentPage';
+import StatusPage from './pages/StatusPage';
+import AdminPanel from './pages/AdminPanel';
 import { OrderProvider } from './context/OrderContext';
 import './styles/App.css';
 
@@ -9,10 +14,17 @@ function App() {
   return (
     <OrderProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<ProductList />} />
-          <Route path="/order" element={<OrderForm />} />
-        </Routes>
+        <Header />
+        <main style={{ paddingBottom: '80px' }}>
+          <Routes>
+            <Route path="/" element={<OrderForm />} />
+            <Route path="/summary" element={<OrderSummary />} />
+            <Route path="/payment" element={<PaymentPage />} />
+            <Route path="/status/:orderId" element={<StatusPage />} />
+            <Route path="/admin" element={<AdminPanel />} />
+          </Routes>
+        </main>
+        <Footer />
       </Router>
     </OrderProvider>
   );
