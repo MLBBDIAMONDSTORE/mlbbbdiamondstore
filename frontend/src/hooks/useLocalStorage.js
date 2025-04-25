8f6e380180151d68bbb07660614fd211356b457f
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:09:26 +0500

    Update useLocalStorage.js

--- frontend/src/hooks/useLocalStorage.js
+++ frontend/src/hooks/useLocalStorage.js
@@ -1,25 +1,16 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-export default function useLocalStorage(key, initialValue) {
+const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.warn(error);
-      return initialValue;
-    }
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
   });
 
-  const setValue = (value) => {
-    try {
-      const valueToStore = value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-      console.warn(error);
-    }
-  };
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(storedValue));
+  }, [key, storedValue]);
 
-  return [storedValue, setValue];
-}
+  return [storedValue, setStoredValue];
+};
+
+export default useLocalStorage;
