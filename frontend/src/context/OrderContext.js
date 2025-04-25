671e37ca9f17d425424c605a350450b96fc14fa4
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 17:25:50 +0500

    Update OrderContext.js

--- frontend/src/context/OrderContext.js
+++ frontend/src/context/OrderContext.js
@@ -3,7 +3,13 @@ import React, { createContext, useState } from 'react';
 export const OrderContext = createContext();
 
 export const OrderProvider = ({ children }) => {
-  const [order, setOrder] = useState(null);
+  const [order, setOrder] = useState({
+    items: [],
+    gameId: '',
+    userId: '',
+    paymentMethod: '',
+    totalAmount: 0,
+  });
 
   return (
     <OrderContext.Provider value={{ order, setOrder }}>
