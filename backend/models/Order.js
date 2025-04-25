ef7a84c1c53cf33ce6a1e043a236ef604b8117d8
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:22:08 +0500

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
-  product: { type: String, required: true },
-  price: { type: Number, required: true },
-  userId: { type: String, required: true },
-  status: { type: String, default: 'pending' },
+  gameId: String,
+  userId: String,
+  amount: Number,
+  paymentMethod: String,
+  status: {
+    type: String,
+    enum: ['pending', 'paid', 'completed'],
+    default: 'pending',
+  },
 }, { timestamps: true });
 
 module.exports = mongoose.model('Order', orderSchema);
