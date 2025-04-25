49d0730ef5a066de0d0e07e6629ca5921e496d1b
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:01:38 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/main.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
+import './styles/main.css';
+import './i18n';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+
 root.render(
   <React.StrictMode>
     <App />
