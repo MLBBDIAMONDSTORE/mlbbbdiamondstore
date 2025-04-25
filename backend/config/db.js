8587009c973c581805ea8272a2f4d716080b4221
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:01:04 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,10 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+  } catch (err) {
+    console.error(err.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB; 
+module.exports = connectDB;
