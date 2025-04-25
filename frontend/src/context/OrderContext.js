ab7a06549e8f7079230672e12b23b44fdf99fccb
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:56:48 +0500

    Update OrderContext.js

--- frontend/src/context/OrderContext.js
+++ frontend/src/context/OrderContext.js
@@ -1,28 +1,33 @@
-import React, { createContext, useContext, useState } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
 const OrderContext = createContext();
 
+export const useOrder = () => useContext(OrderContext);
+
 export const OrderProvider = ({ children }) => {
-  const [cart, setCart]         = useState([]);
-  const [mlbbId, setMlbbId]     = useState('');
-  const [zone, setZone]         = useState('');
+  const [cart, setCart] = useState([]);
+  const [mlbbId, setMlbbId] = useState('');
+  const [zoneId, setZoneId] = useState('');
   const [nickname, setNickname] = useState('');
 
-  const addToCart = (product) => {
-    setCart(prev => [...prev, product]);
-  };
-
+  const addToCart = (product) => setCart((prev) => [...prev, product]);
   const clearCart = () => setCart([]);
 
   return (
-    <OrderContext.Provider value={{
-      cart, addToCart, clearCart,
-      mlbbId, setMlbbId, zone, setZone,
-      nickname, setNickname
-    }}>
+    <OrderContext.Provider
+      value={{
+        cart,
+        addToCart,
+        clearCart,
+        mlbbId,
+        setMlbbId,
+        zoneId,
+        setZoneId,
+        nickname,
+        setNickname,
+      }}
+    >
       {children}
     </OrderContext.Provider>
   );
 };
-
-export const useOrder = () => useContext(OrderContext);
