d2c4c25d1df983c89a94e26d4afe57bf827042d1
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 18:46:02 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import OrderForm from './pages/OrderForm';
-import OrderSummary from './pages/OrderSummary';
-import PaymentPage from './pages/PaymentPage';
-import StatusPage from './pages/StatusPage';
-import AdminPanel from './pages/AdminPanel';
-import Header from './components/Layout/Header';
-import Footer from './components/Layout/Footer';
+import HomePage from './pages/HomePage';
+import ProductsPage from './pages/ProductsPage';
+import './i18n';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
       <Routes>
-        <Route path="/" element={<OrderForm />} />
-        <Route path="/summary" element={<OrderSummary />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/status" element={<StatusPage />} />
-        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/products" element={<ProductsPage />} />
       </Routes>
-      <Footer />
     </Router>
   );
 }
