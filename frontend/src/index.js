6a1afaeadd1447fec1a8e30353fb840e1c9a938d
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:36:12 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.css';
-import './i18n';
+import { BrowserRouter } from 'react-router-dom';
+import { OrderProvider } from './context/OrderContext';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <OrderProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </OrderProvider>
 );
