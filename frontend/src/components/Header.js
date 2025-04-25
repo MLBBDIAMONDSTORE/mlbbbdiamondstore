eb4257e9bdf1e39190164d42c5508b3ae796a941
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:16:43 +0500

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,1 +1,20 @@
+import React, { useContext } from 'react';
+import LanguageSelector from './LanguageSelector';
+import { OrderContext } from '../context/OrderContext';
+import logo from '../assets/logo.png';
 
+const Header = () => {
+  const { nickname } = useContext(OrderContext);
+
+  return (
+    <header className="header">
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="header-info">
+        <span className="nickname">{nickname}</span>
+        <LanguageSelector />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
