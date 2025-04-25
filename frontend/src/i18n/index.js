82cb96448ec61a97564d2e0c97cc1de0f1016b0e
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:57:25 +0500

    Update index.js

--- frontend/src/i18n/index.js
+++ frontend/src/i18n/index.js
@@ -4,15 +4,13 @@ import uz from './uz.json';
 import ru from './ru.json';
 import en from './en.json';
 
-const resources = {
-  uz: { translation: uz },
-  ru: { translation: ru },
-  en: { translation: en },
-};
-
 i18n.use(initReactI18next).init({
-  resources,
-  lng: localStorage.getItem('language') || 'uz',
+  resources: {
+    uz: { translation: uz },
+    ru: { translation: ru },
+    en: { translation: en },
+  },
+  lng: 'uz',
   fallbackLng: 'uz',
   interpolation: { escapeValue: false },
 });
