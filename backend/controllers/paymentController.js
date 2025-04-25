4ef618c296e8c88b618d040eec187289a7347c80
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 20:59:40 +0500

    Delete backend/controllers/paymentController.js

--- backend/controllers/paymentController.js
+++
@@ -1,9 +0,0 @@
-exports.processPayment = async (req, res) => {
-  const { cardNumber, amount } = req.body;
-
-  if (!cardNumber || !amount) {
-    return res.status(400).json({ error: 'Missing fields' });
-  }
-
-  return res.json({ message: 'Payment processed successfully' });
-};
