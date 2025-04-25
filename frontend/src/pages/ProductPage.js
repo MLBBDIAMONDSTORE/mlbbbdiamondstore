9cebae2f8f631de018f674c3eb0d2ea15f3111af
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:59:58 +0500

    Create ProductPage.js

---
+++ frontend/src/pages/ProductPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../components/ProductList';
+
+const ProductPage = () => (
+  <div>
+    <Header />
+    <ProductList />
+  </div>
+);
+
+export default ProductPage;
