fddce7a3f9d25689aac4a71f24b257f6f7c798b0
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:18:07 +0500

    Update OrderSummary.js

--- frontend/src/components/OrderSummary.js
+++ frontend/src/components/OrderSummary.js
@@ -1,1 +1,26 @@
+import React, { useContext } from 'react';
+import { OrderContext } from '../context/OrderContext';
 
+const OrderSummary = () => {
+  const { selectedProducts, nickname } = useContext(OrderContext);
+  const total = selectedProducts.reduce((sum, p) => sum + p.price, 0).toFixed(2);
+
+  return (
+    <div className="order-summary">
+      <h3>Order Summary</h3>
+      <ul>
+        {selectedProducts.map((item, index) => (
+          <li key={index}>
+            {item.name} - ${item.price}
+          </li>
+        ))}
+      </ul>
+      <div className="total">
+        <strong>Total:</strong> ${total}
+      </div>
+      {nickname && <button className="pay-btn">PAY</button>}
+    </div>
+  );
+};
+
+export default OrderSummary;
