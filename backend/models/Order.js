d107ff80ee9b743a2353ce8c1a0d9385c4060f84
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 23:08:47 +0500

    Update Order.js

--- backend/models/Order.js
+++ backend/models/Order.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
-  nickname: { type: String, required: true },
-  userId: { type: String, required: true },
-  zoneId: { type: String, required: true },
-  productId: { type: String, required: true },
-  diamonds: { type: Number, required: true },
-  price: { type: Number, required: true },
-  status: { type: String, default: 'pending' },
-  createdAt: { type: Date, default: Date.now },
+  mlbbId: String,
+  zoneId: String,
+  nickname: String,
+  products: Array,
+  createdAt: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Order', OrderSchema);
