09918c80d4893c3591792ec792c38e3d91d7c277
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 11:24:33 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ProductList from './components/ProductList';
-import OrderForm from './components/OrderForm';
+import OrderSummary from './components/OrderSummary';
 import PaymentPage from './components/PaymentPage';
 import AdminPanel from './components/AdminPanel';
-import OrderSummary from './components/OrderSummary';
 import StatusPage from './components/StatusPage';
 import { OrderProvider } from './context/OrderContext';
 import './styles/App.css';
@@ -15,7 +14,6 @@ const App = () => {
       <Router>
         <Routes>
           <Route path="/" element={<ProductList />} />
-          <Route path="/order" element={<OrderForm />} />
           <Route path="/summary" element={<OrderSummary />} />
           <Route path="/pay" element={<PaymentPage />} />
           <Route path="/admin" element={<AdminPanel />} />
