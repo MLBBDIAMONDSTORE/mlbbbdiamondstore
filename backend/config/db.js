fac07f2881e90e4c8cc67e444422f4f2caec0422
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:05:23 +0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,18 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('MongoDB connected');
-  } catch (err) {
-    console.error(err.message);
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
     process.exit(1);
   }
 };
 
-module.exports = connectDB;
+export default connectDB;
