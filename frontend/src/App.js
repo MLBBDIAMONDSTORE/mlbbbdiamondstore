221c2cc0d987a8674b112c399724b675cc8c8a7a
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 23:39:55 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,54 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductPage from './pages/ProductPage';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { OrderProvider, OrderContext } from './context/OrderContext';
+import Header from './components/Header';
+
+import LoginPage from './pages/LoginPage';
+import ProductsPage from './pages/ProductsPage';
 import OrderViewPage from './pages/OrderViewPage';
 import PayPage from './pages/PayPage';
-import { OrderProvider } from './context/OrderContext';
 
-const App = () => (
-  <OrderProvider>
-    <Router>
+function App() {
+  return (
+    <OrderProvider>
+      <Header />
       <Routes>
-        <Route path="/" element={<ProductPage />} />
-        <Route path="/order" element={<OrderViewPage />} />
-        <Route path="/pay" element={<PayPage />} />
+        <Route path="/" element={<LoginPage />} />
+        {/* Agar user tekshiruvi o‘tilmagan bo‘lsa, login sahifasiga qaytar */}
+        <Route
+          path="/products"
+          element={
+            <RequireAuth>
+              <ProductsPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/order"
+          element={
+            <RequireAuth>
+              <OrderViewPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/pay"
+          element={
+            <RequireAuth>
+              <PayPage />
+            </RequireAuth>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </Router>
-  </OrderProvider>
-);
+    </OrderProvider>
+  );
+}
+
+// router guard
+function RequireAuth({ children }) {
+  const { user } = React.useContext(OrderContext);
+  return user ? children : <Navigate to="/" />;
+}
 
 export default App;
