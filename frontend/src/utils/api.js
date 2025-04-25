1fe964ad6871d5a1b7605d09110ebdb465f2f142
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:58:01 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,9 +1,12 @@
-export const validateNickname = async (nickname) => {
+export const validateMLBB = async (mlbbId, zoneId) => {
+  const param = encodeURIComponent(`${mlbbId}(${zoneId})`);
   try {
-    const response = await fetch(`https://api.isan.eu.org/nickname/ml?id=${nickname}`);
-    const data = await response.json();
-    return data.status === true;
-  } catch (error) {
-    return false;
+    const res = await fetch(
+      `https://api.isan.eu.org/nickname/ml?id=${param}`
+    );
+    const data = await res.json();
+    return data.username || null;
+  } catch {
+    return null;
   }
 };
