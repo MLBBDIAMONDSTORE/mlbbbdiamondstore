22528253efc9880d0fef9213c2746a64a1b98260
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 18:35:50 +0500

    Create Products.jsx

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Products({ nickname }) {
+  return (
+    <div>
+      <Navbar nickname={nickname} />
+      {/* Mahsulotlar ro'yxati */}
+    </div>
+  );
+}
+
+export default Products;
