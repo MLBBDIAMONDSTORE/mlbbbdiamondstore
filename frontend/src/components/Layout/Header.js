bd861c5e0220e3cec4b92c4e087b28d3149dd3ba
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:25:52 +0500

    Update Header.js

---
+++ frontend/src/components/Layout/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+
+const Header = () => (
+  <header>
+    <img src={logo} alt="MLBB Diamond Store" />
+    <h1>MLBB Diamond Store</h1>
+  </header>
+);
+
+export default Header;
