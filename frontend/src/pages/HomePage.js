1569811aacf24d933fb3ab0218434386930dfdbc
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:16:22 +0500

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import Header from '../components/Header';
+import ProductList from '../components/ProductList';
+import OrderForm from '../components/OrderForm';
+import OrderSummary from '../components/OrderSummary';
 
+const HomePage = () => {
+  return (
+    <div className="container">
+      <Header />
+      <ProductList />
+      <OrderForm />
+      <OrderSummary />
+    </div>
+  );
+};
+
+export default HomePage;
