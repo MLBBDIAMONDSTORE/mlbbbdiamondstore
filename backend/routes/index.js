373a8bad1dba31f0ed1240b64edfd3ea39047e91
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:22:30 +0500

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const orderRoutes = require('./orderRoutes');
-
 const router = express.Router();
 
+const orderRoutes = require('./orderRoutes');
+const paymentRoutes = require('./paymentRoutes');
+const adminRoutes = require('./adminRoutes');
+
 router.use('/orders', orderRoutes);
+router.use('/payments', paymentRoutes);
+router.use('/admin', adminRoutes);
 
 module.exports = router;
