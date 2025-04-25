43cb0a4fb8391af31c2a997b020731d52015b8c7
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:10:27 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { OrderProvider } from './context/OrderContext';
+import './i18n';
 import './styles/main.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
