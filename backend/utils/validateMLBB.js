2556329ce8016c08911cdbb6bcc469f622251a29
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:01:58 +0500

    Update validateMLBB.js

--- backend/utils/validateMLBB.js
+++ backend/utils/validateMLBB.js
@@ -1,3 +1,16 @@
-exports.validateCard = (cardNumber) => {
-  return /^[0-9]{16}$/.test(cardNumber);
+const axios = require('axios');
+
+const validateMLBB = async (userId, zoneId) => {
+  try {
+    const response = await axios.get(`https://api.isan.eu.org/nickname/ml?id=${userId}&zone=${zoneId}`);
+    if (response.data && response.data.nickname) {
+      return { valid: true, nickname: response.data.nickname };
+    } else {
+      return { valid: false, message: 'Nickname not found' };
+    }
+  } catch (error) {
+    return { valid: false, message: 'Validation error' };
+  }
 };
+
+module.exports = validateMLBB;
