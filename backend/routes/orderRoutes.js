690cca9b532b06ed49cda7ec8b7b09853b43ce13
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:22:41 +0500

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { createOrder, getOrders } = require('../controllers/orderController');
+const { createOrder, getOrderById, getAllOrders } = require('../controllers/orderController');
 
 const router = express.Router();
 
 router.post('/', createOrder);
-router.get('/', getOrders);
+router.get('/:id', getOrderById);
+router.get('/', getAllOrders);
 
 module.exports = router;
