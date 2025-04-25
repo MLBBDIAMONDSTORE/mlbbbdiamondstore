e08357de84f2e4914ebd0bad5f06e46857e91f14
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:06:29 +0500

    Create productController.js

---
+++ backend/controllers/productController.js
@@ -0,0 +1,10 @@
+import Product from '../models/Product.js';
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch products' });
+  }
+};
