212f9afacbd9d9c77d8081baae1a03b823c23271
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 15:12:14 +0500

    Update StatusPage.js

--- frontend/src/components/StatusPage.js
+++ frontend/src/components/StatusPage.js
@@ -1,14 +1,4 @@
 import React from 'react';
-import '../styles/OrderStatus.css';
+import '../styles/PaymentPage.css';
 
-const StatusPage = () => {
-  return (
-    <div className="status-page">
-      <h2>Order Status</h2>
-      <p>Order ID: <strong>#123456</strong></p>
-      <p>Status: Pending (waiting for admin approval)</p>
-    </div>
-  );
-};
-
-export default StatusPage;
+const 
