28598b264dba55f18a4ba3ac94817b676a1c583f
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:53:56 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import { OrderProvider } from './context/OrderContext';
 import './styles/main.css';
-import './i18n';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <OrderProvider>
+  <React.StrictMode>
     <App />
-  </OrderProvider>
+  </React.StrictMode>
 );
