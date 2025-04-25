c29d4becfaad73f5ac38a27ccc5f3cbef5f22633
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 21:03:36 +0500

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,1 +1,10 @@
+import axios from 'axios';
 
+export const createOrder = async (orderData) => {
+  try {
+    const response = await axios.post('/api/orders', orderData);
+    return response.data;
+  } catch (error) {
+    throw error.response.data;
+  }
+};
