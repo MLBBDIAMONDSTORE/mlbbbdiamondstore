5884764d1d49a6bbac08aa148cc2e69fb86d7edf
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 15:33:02 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ProductList from './components/ProductList';
-import OrderForm from './components/OrderForm';
-import PaymentPage from './components/PaymentPage';
-import AdminPanel from './components/AdminPanel';
-import OrderSummary from './components/OrderSummary';
-import StatusPage from './components/StatusPage';
-import { OrderProvider } from './context/OrderContext';
-import './styles/App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductList from "./components/ProductList";
+import OrderForm from "./components/OrderForm";
+import { OrderProvider } from "./context/OrderContext";
+import "./styles/App.css";
 
-const App = () => {
+function App() {
   return (
     <OrderProvider>
       <Router>
         <Routes>
           <Route path="/" element={<ProductList />} />
           <Route path="/order" element={<OrderForm />} />
-          <Route path="/summary" element={<OrderSummary />} />
-          <Route path="/pay" element={<PaymentPage />} />
-          <Route path="/admin" element={<AdminPanel />} />
-          <Route path="/status" element={<StatusPage />} />
         </Routes>
       </Router>
     </OrderProvider>
   );
-};
+}
 
 export default App;
