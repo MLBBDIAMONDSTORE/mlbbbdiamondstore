23a64efb62252dfb100998cdfaa2bf2cc99710ed
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:06:53 +0500

    Update LanguageSelector.js

--- frontend/src/components/LanguageSelector.js
+++ frontend/src/components/LanguageSelector.js
@@ -4,18 +4,18 @@ import useLocalStorage from '../hooks/useLocalStorage';
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
-  const [lang, setLang] = useLocalStorage('language', 'uz');
+  const [language, setLanguage] = useLocalStorage('language', 'uz');
 
   const changeLanguage = (lng) => {
-    setLang(lng);
     i18n.changeLanguage(lng);
+    setLanguage(lng);
   };
 
   return (
     <div className="language-selector">
       <button onClick={() => changeLanguage('uz')}>🇺🇿</button>
       <button onClick={() => changeLanguage('ru')}>🇷🇺</button>
-      <button onClick={() => changeLanguage('en')}>🇬🇧</button>
+      <button onClick={() => changeLanguage('en')}>🇺🇸</button>
     </div>
   );
 };
