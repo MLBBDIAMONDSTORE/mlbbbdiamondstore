f9499697a7fc069eea7b2365b11059cdd5e433ff
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 22:58:21 +0500

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,19 +3,17 @@ import LanguageSelector from './LanguageSelector';
 import logo from '../assets/logo.png';
 import '../styles/main.css';
 
-const Header = ({ nickname }) => {
-  return (
-    <header className="header">
-      <div className="logo-name">
-        <img src={logo} alt="Logo" className="logo" />
-        <h1 className="site-name">MLBB Diamond Store</h1>
-      </div>
-      <div className="right-side">
-        {nickname && <span className="nickname">{nickname}</span>}
-        <LanguageSelector />
-      </div>
-    </header>
-  );
-};
+const Header = ({ nickname }) => (
+  <header className="header">
+    <div className="logo-name">
+      <img src={logo} alt="Logo" className="logo" />
+      <h1 className="site-name">MLBB Diamond Store</h1>
+    </div>
+    <div className="right-side">
+      {nickname && <span className="nickname">{nickname}</span>}
+      <LanguageSelector />
+    </div>
+  </header>
+);
 
 export default Header;
