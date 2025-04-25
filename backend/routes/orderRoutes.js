153a0104e3734aca7688ff5bef1f6d61becfcdbe
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-25 16:13:27 +0500

    Update orderRoutes.js

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,24 +1,9 @@
 const express = require('express');
+const { createOrder, getOrders } = require('../controllers/orderController');
+
 const router = express.Router();
-const {
-  createOrder,
-  getOrderStatus,
-  getAllOrders,
-  approveOrder,
-  rejectOrder,
-  submitRefundCard,
-  confirmRefund
-} = require('../controllers/orderController');
 
-// Public
 router.post('/', createOrder);
-router.get('/:id/status', getOrderStatus);
-
-// Admin
-router.get('/', getAllOrders);
-router.patch('/:id/approve', approveOrder);
-router.patch('/:id/reject', rejectOrder);
-router.post('/:id/refund-card', submitRefundCard);
-router.patch('/:id/refund-confirm', confirmRefund);
+router.get('/', getOrders);
 
 module.exports = router;
