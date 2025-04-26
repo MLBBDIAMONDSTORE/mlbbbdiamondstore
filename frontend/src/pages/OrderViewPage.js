0d3401b6def8abbd499a0b394d7f2d051d62d8d1
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:32:27 +0500

    Update OrderViewPage.js

--- frontend/src/pages/OrderViewPage.js
+++ frontend/src/pages/OrderViewPage.js
@@ -1,23 +1,15 @@
-import React, { useContext } from 'react';
-import { OrderContext } from '../context/OrderContext';
+import React from 'react';
 import OrderSummary from '../components/OrderSummary';
 import { useNavigate } from 'react-router-dom';
-import '../styles/main.css';
 
 export default function OrderViewPage() {
-  const { cart } = useContext(OrderContext);
   const navigate = useNavigate();
 
-  const handleConfirm = () => {
-    navigate('/pay');
-  };
-
   return (
-    <div className="page order-view-page">
-      <h1 className="page-title">Buyurtmani Ko‘rish</h1>
+    <div className="page order-page">
       <OrderSummary />
-      <button onClick={handleConfirm} className="confirm-button">
-        Buyurtmani Tasdiqlash
+      <button className="next-btn" onClick={() => navigate('/pay')}>
+        Buyurtmani tasdiqlash
       </button>
     </div>
   );
