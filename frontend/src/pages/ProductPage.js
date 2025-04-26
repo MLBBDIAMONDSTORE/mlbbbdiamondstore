1431ef750b66948e6446b7ac124ff12ea5f89160
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 11:18:59 +0500

    Update ProductPage.js

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -4,18 +4,18 @@ import ProductCard from '../components/ProductCard';
 import '../styles/main.css';
 
 const products = [
-    { id: 1, name: '86 Diamonds', price: 00000, emoji: '💎' },
-  { id: 2, name: '172 Diamonds', price: 00000, emoji: '💎' },
-  { id: 3, name: '257 Diamonds', price: 00000, emoji: '💎' },
-  { id: 4, name: '706 Diamonds', price: 00000, emoji: '💎' },
-  { id: 5, name: '2195 Diamonds', price: 00000, emoji: '💎' },
-  { id: 6, name: '2398 Diamonds', price: 00000, emoji: '💎' },
-  { id: 7, name: '3688 Diamonds', price: 00000, emoji: '💎' },
-  { id: 8, name: '5532 Diamonds', price: 00000, emoji: '💎' },
-  { id: 9, name: '6042 Diamonds', price: 00000, emoji: '💎' },
-  { id: 10, name: '9288 Diamonds', price: 00000, emoji: '💎' },
-  { id: 11, name: 'Weekly Pass', price: 00000, emoji: '💎' },
-  { id: 12, name: 'Twilight Pass', price: 00000, emoji: '💎' },
+    { id: 1, name: '86 Diamonds', price: 100000, emoji: '💎' },
+  { id: 2, name: '172 Diamonds', price: 100000, emoji: '💎' },
+  { id: 3, name: '257 Diamonds', price: 100000, emoji: '💎' },
+  { id: 4, name: '706 Diamonds', price: 100000, emoji: '💎' },
+  { id: 5, name: '2195 Diamonds', price: 100000, emoji: '💎' },
+  { id: 6, name: '2398 Diamonds', price: 100000, emoji: '💎' },
+  { id: 7, name: '3688 Diamonds', price: 100000, emoji: '💎' },
+  { id: 8, name: '5532 Diamonds', price: 100000, emoji: '💎' },
+  { id: 9, name: '6042 Diamonds', price: 100000, emoji: '💎' },
+  { id: 10, name: '9288 Diamonds', price: 100000, emoji: '💎' },
+  { id: 11, name: 'Weekly Pass', price: 100000, emoji: '💎' },
+  { id: 12, name: 'Twilight Pass', price: 100000, emoji: '💎' },
 ];
 
 const ProductPage = () => {
