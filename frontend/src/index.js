88063849d36cd1a6b7feb5985da33b05eb11407d
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 09:37:12 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,8 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/main.css';
 
-const root = document.getElementById('root');
-ReactDOM.render(<App />, root);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
