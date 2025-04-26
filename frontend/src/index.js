8d91aab6fe9c837ec30cfc2cbe523071021b0d18
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 08:50:13 +0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,8 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/main.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import './i18n'; // i18next inizializatsiyasi
+import './styles/main.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
