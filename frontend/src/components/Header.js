f9d51237c400670da7ef31bff20342f9b97a6f75
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:49:19 +0500

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,15 +1,18 @@
 import React, { useContext } from 'react';
-import LanguageSelector from './LanguageSelector';
 import { OrderContext } from '../context/OrderContext';
-import '../styles/main.css';
+import LanguageSelector from './LanguageSelector';
+import logo from '../assets/logo.png';
 
 export default function Header() {
   const { nickname } = useContext(OrderContext);
 
   return (
     <header className="header">
-      <div className="header-left">MLBB Diamond Store</div>
-      <div className="header-right">
+      <div className="left">
+        <img src={logo} alt="Logo" className="logo" />
+        <span className="site-title">MLBB Diamond Store</span>
+      </div>
+      <div className="right">
         {nickname && <span className="nickname">{nickname}</span>}
         <LanguageSelector />
       </div>
