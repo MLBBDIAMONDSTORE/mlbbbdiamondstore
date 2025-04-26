40602ba033cff3a6f83d49e0c2fa8398d8ace884
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:47:46 +0500

    Update OrderViewPage.js

--- frontend/src/pages/OrderViewPage.js
+++ frontend/src/pages/OrderViewPage.js
@@ -1,49 +1,16 @@
-import React, { useContext } from 'react';
-import { OrderContext } from '../context/OrderContext';
-import '../styles/OrderViewPage.css';
+import React from 'react';
+import OrderSummary from '../components/OrderSummary';
+import { useNavigate } from 'react-router-dom';
 
-const taglines = {
-  Fries: 'Po-ta-toes',
-  Hotdog: '0% dog, 100% hot',
-  Taco: 'Mucho más',
-  Pizza: 'That’s amore',
-  Donut: 'Hole included',
-  Popcorn: 'Lights, camera, corn',
-  Coke: 'Come of shame',
-  Icecream: 'Cone of shame',
-  Cookie: 'Milk’s favorite',
-  Flan: 'Flan-tastic',
-};
-
-const OrderViewPage = () => {
-  const { cart, getTotalPrice } = useContext(OrderContext);
-
+export default function OrderViewPage() {
+  const navigate = useNavigate();
   return (
-    <div className="order-page">
-      <h2 className="order-title">YOUR ORDER</h2>
-      <div className="order-list">
-        {cart.map((item) => (
-          <div key={item.id} className="order-item">
-            <div className="order-left">
-              <div className="order-emoji">{item.emoji}</div>
-              <div>
-                <div className="order-name">
-                  {item.name} <span className="order-quantity">{item.quantity}x</span>
-                </div>
-                <div className="order-tagline">{taglines[item.name]}</div>
-              </div>
-            </div>
-            <div className="order-price">
-              ${(item.price * item.quantity).toFixed(2)}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div className="pay-button-wrapper">
-        <button className="pay-button">PAY ${getTotalPrice().toFixed(2)}</button>
-      </div>
+    <div className="container">
+      <h1>Buyurtmangiz</h1>
+      <OrderSummary />
+      <button className="confirm-btn" onClick={() => navigate('/pay')}>
+        Buyurtmani tasdiqlash
+      </button>
     </div>
   );
-};
-
-export default OrderViewPage;
+}
