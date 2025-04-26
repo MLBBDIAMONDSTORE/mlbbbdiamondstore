9b7f3f739e2a1eb5cbe0715bc18d90215f24a062
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:32:40 +0500

    Update PayPage.js

--- frontend/src/pages/PayPage.js
+++ frontend/src/pages/PayPage.js
@@ -1,25 +1,10 @@
-import React, { useContext } from 'react';
-import { OrderContext } from '../context/OrderContext';
-import '../styles/main.css';
+import React from 'react';
 
 export default function PayPage() {
-  const { cart, user } = useContext(OrderContext);
-
-  const totalPrice = cart.reduce((sum, item) => sum + item.price, 0).toFixed(2);
-
   return (
     <div className="page pay-page">
-      <h1 className="page-title">To‘lov Sahifasi</h1>
-      <p className="summary-text">Foydalanuvchi: {user.nickname}</p>
-      <ul className="pay-list">
-        {cart.map((item) => (
-          <li key={item.id}>
-            {item.name} - ${item.price.toFixed(2)}
-          </li>
-        ))}
-      </ul>
-      <p className="total-text">Umumiy: ${totalPrice}</p>
-      <button className="pay-button">To‘lovni amalga oshirish</button>
+      <h2>To‘lov sahifasi</h2>
+      <p>To‘lov interfeysi bu yerda bo‘ladi.</p>
     </div>
   );
 }
