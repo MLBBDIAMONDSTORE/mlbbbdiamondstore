507ca951371e1681783bc5020bc2503c386b0fff
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:50:35 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
-// src/App.js
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import ProductPage from './pages/ProductPage';
 import VerifyPage from './pages/VerifyPage';
 import OrderViewPage from './pages/OrderViewPage';
 import PayPage from './pages/PayPage';
-import Header from './components/Header';
 import { OrderProvider } from './context/OrderContext';
 import './styles/main.css';
 
@@ -13,12 +11,12 @@ function App() {
   return (
     <OrderProvider>
       <Router>
-        <Header />
         <Routes>
           <Route path="/" element={<ProductPage />} />
           <Route path="/verify" element={<VerifyPage />} />
           <Route path="/order" element={<OrderViewPage />} />
           <Route path="/pay" element={<PayPage />} />
+          <Route path="*" element={<Navigate to="/" />} />
         </Routes>
       </Router>
     </OrderProvider>
