d1b6d9df4ac2f2b4e9e06af90cb7b39f03a9f203
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-26 10:25:02 +0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,24 +6,40 @@ import ProductPage from './pages/ProductPage';
 import VerifyPage from './pages/VerifyPage';
 import OrderViewPage from './pages/OrderViewPage';
 import PayPage from './pages/PayPage';
-import { OrderContext } from './context/OrderContext';
+import { OrderProvider, OrderContext } from './context/OrderContext';
 
 function ProtectedRoute({ children }) {
-  const { user } = React.useContext(OrderContext);
-  return user?.nickname ? children : <Navigate to="/verify" replace />;
+  const { nickname } = React.useContext(OrderContext);
+  return nickname ? children : <Navigate to="/verify" replace />;
 }
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<ProductPage />} />
-        <Route path="/verify" element={<VerifyPage />} />
-        <Route path="/order" element={<ProtectedRoute><OrderViewPage /></ProtectedRoute>} />
-        <Route path="/pay" element={<ProtectedRoute><PayPage /></ProtectedRoute>} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </Router>
+    <OrderProvider>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="/verify" element={<VerifyPage />} />
+          <Route
+            path="/order"
+            element={
+              <ProtectedRoute>
+                <OrderViewPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/pay"
+            element={
+              <ProtectedRoute>
+                <PayPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </Router>
+    </OrderProvider>
   );
 }
