633fcfc30d38eb3c5126139bff6459a23502497d
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-27 07:24:32 +0000

    f

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { LanguageContext } from '../contexts/LanguageContext';
 import '../styles/ProductCard.css';
 
