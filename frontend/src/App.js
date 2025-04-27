a08cbb471a5e21421814d18f435508ad9a232243
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-27 06:05:54 +0000

    g

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from './contexts/ThemeContext';
+import { LanguageProvider } from './contexts/LanguageContext';
+import { OrderProvider } from './contexts/OrderContext';
+import ProductPage from './pages/ProductPage';
+import OrderPage from './pages/OrderPage'; // Hozircha bo'sh sahifa
+
+function App() {
+  return (
+    <ThemeProvider>
+      <LanguageProvider>
+        <OrderProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<ProductPage />} />
+              <Route path="/order" element={<OrderPage />} />
+            </Routes>
+          </Router>
+        </OrderProvider>
+      </LanguageProvider>
+    </ThemeProvider>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/OrderPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderPage = () => {
+  return (
+    <div style={{ textAlign: 'center', paddingTop: '100px', color: '#fff' }}>
+      <h2>Order Page (Hozircha bo'sh)</h2>
+    </div>
+  );
+};
+
+export default OrderPage;
