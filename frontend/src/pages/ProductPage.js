124c2be9fe1208c91bda0b917d3c05fde409deb0
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-27 13:01:09 +0500

    Update ProductPage.js

--- frontend/src/pages/ProductPage.js
+++ frontend/src/pages/ProductPage.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import ProductCard from '../components/ProductCard';
+import ProductList from '../components/ProductList';
+import products from '../data/products';
 import '../styles/ProductPage.css';
-import { products } from '../data/products';
 
 const ProductPage = () => {
   return (
     <div className="product-page">
-      <h1 className="title">Select Your Diamonds</h1>
-      <div className="product-grid">
-        {products.map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
-      </div>
+      <h1>Diamonds Store</h1>
+      <ProductList products={products} onAdd={() => {}} />
     </div>
   );
 };
