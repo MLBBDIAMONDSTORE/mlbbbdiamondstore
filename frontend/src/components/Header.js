4519428ea7176486e75de6f4d17b05e3f968c866
MLBBDIAMONDSTORE <murodyago@gmail.com>
2025-04-27 12:59:36 +0500

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,32 +1,11 @@
-// src/components/Header.js
-import React, { useContext } from 'react';
-import { ThemeContext } from '../contexts/ThemeContext';
-import { LanguageContext } from '../contexts/LanguageContext';
+import React from 'react';
 import '../styles/Header.css';
 
-const Header = () => {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-  const { language, changeLanguage } = useContext(LanguageContext);
-
+const Header = ({ nickname }) => {
   return (
     <header className="header">
-      <div className="header-left">
-        MLBB Diamond Store
-      </div>
-      <div className="header-right">
-        <button className="theme-toggle" onClick={toggleTheme}>
-          {theme === 'light' ? '🌞' : '🌙'}
-        </button>
-        <select
-          className="language-select"
-          value={language}
-          onChange={(e) => changeLanguage(e.target.value)}
-        >
-          <option value="uz">UZ</option>
-          <option value="ru">RU</option>
-          <option value="en">EN</option>
-        </select>
-      </div>
+      <div className="header-left">MLBB Diamond Store</div>
+      <div className="header-right">{nickname}</div>
     </header>
   );
 };
